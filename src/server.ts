e92913ad9b92c81da492465abfc27d00a1fc312d
Mateus Henrique <mateushenriquechaves1@gmail.com>
2023-04-14 04:11:02 -0300

    refactor: changing middlewares to server file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import express from 'express'
 
+import TaskRouter from './modules/task/task.router'
+import TaskController from './modules/task/task.controller'
+import TaskService from './modules/task/task.service'
+import TaskRepository from './modules/task/task.repository'
+
+const taskControllerWithDependencies = new TaskController(new TaskService(new TaskRepository()))
+
 const app = express()
 
+app.use(express.json())
+app.use(new TaskRouter(taskControllerWithDependencies).getRouter())
 
 export default app
